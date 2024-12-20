d93d16febdf5304f6930f1632785d097a7ed6998
nojyto <nojusskirkevicius@gmail.com>
2024-12-21 00:27:12 +0200

    Refactor Electron main process and remove unused resource management code

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,22 +1,20 @@
-import { app, BrowserWindow, ipcMain } from "electron";
+import { app, BrowserWindow } from "electron";
 import path from "path";
-import { getPreloadPath } from "./pathResolver.js";
-import { getSystemInfo, pollResources } from './resourceManagers.js';
-import { isDev } from "./util.js";
+import { getPreloadPath, isDev } from "./util.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
-		webPreferences: {
-			preload: getPreloadPath(),
-		},
-	});
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
+
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123");
+    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
   }
-
-	pollResources(mainWindow);
-
-	ipcMain.handle('getStaticData', () => getSystemInfo());
 });
--- src/electron/preload.cts
+++ src/electron/preload.cts
@@ -1,10 +1,5 @@
 const electron = require("electron");
 
 electron.contextBridge.exposeInMainWorld("electron", {
-  subscribeStatistics: (callback: (statistics: any) => void) => {
-    electron.ipcRenderer.on("statistics", (_, statistics) =>
-      callback(statistics)
-    );
-  },
-  getStaticData: () => electron.ipcRenderer.invoke("getStaticData"),
+
 });
