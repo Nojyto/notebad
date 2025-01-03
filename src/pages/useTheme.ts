f3fa9a01ad99996f5e62769ddd598f2dd3dc90d9
WhiteRain3 <sadykovas.danielius@gmail.com>
2025-01-03 02:47:21 +0200

    bbz

---
+++ src/pages/useTheme.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export const useTheme = () => {
+  const [theme, setTheme] = useState(() =>
+    window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  );
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (theme === 'dark') {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
+  return { theme, toggleTheme };
+};
