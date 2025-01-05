ef24fe943370083f5a37031cc96216ce34e9aad4
nojyto <nojusskirkevicius@gmail.com>
2025-01-05 21:59:34 +0200

    Add tailwind-scrollbar plugin and implement editor shortcuts for tab management

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbarPlugin from 'tailwind-scrollbar';
+
 export default {
   content: [
     './index.html',
@@ -49,5 +51,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [scrollbarPlugin],
 };
