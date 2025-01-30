0835e05dbde01b0891cd32b850b1502b96495799
nojyto <nojusskirkevicius@gmail.com>
2025-01-30 18:30:31 +0200

    Refactor SearchBar component for improved readability and consistency; update string delimiters to double quotes

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,12 +1,12 @@
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
-import { useState } from 'react';
-import AppMenu from '../components/AppMenu';
-import ConfirmDialog from '../components/ConfirmDialog';
-import SearchBar from '../components/SearchBar';
-import StatusBar from '../components/StatusBar';
-import { useEditorShortcuts } from '../hooks/useEditorShortcuts';
-import { useEditorState } from '../hooks/useEditorState';
-import { TabData } from '../types/types';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { useState } from "react";
+import AppMenu from "../components/AppMenu";
+import ConfirmDialog from "../components/ConfirmDialog";
+import SearchBar from "../components/SearchBar";
+import StatusBar from "../components/StatusBar";
+import { useEditorShortcuts } from "../hooks/useEditorShortcuts";
+import { useEditorState } from "../hooks/useEditorState";
+import { TabData } from "../types/types";
 
 const EditorPage = () => {
   const {
@@ -38,8 +38,11 @@ const EditorPage = () => {
       textArea.focus();
       textArea.setSelectionRange(index, index + length);
 
-      const startLine = textArea.value.substring(0, index).split('\n').length - 1;
-      const lineHeight = parseFloat(getComputedStyle(textArea).lineHeight || '20px');
+      const startLine =
+        textArea.value.substring(0, index).split("\n").length - 1;
+      const lineHeight = parseFloat(
+        getComputedStyle(textArea).lineHeight || "20px"
+      );
       const scrollPosition = startLine * lineHeight - textArea.clientHeight / 2;
 
       textArea.scrollTop = Math.max(scrollPosition, 0);
@@ -53,19 +56,19 @@ const EditorPage = () => {
       <AppMenu
         handleMenuClick={(menuItem) => {
           switch (menuItem) {
-            case 'New File':
+            case "New File":
               addTab();
               break;
-            case 'Open File':
+            case "Open File":
               openFile();
               break;
-            case 'Save File':
+            case "Save File":
               saveTab(activeIndex);
               break;
-            case 'Save File As':
+            case "Save File As":
               saveAsTab(activeIndex);
               break;
-            case 'Close Tab':
+            case "Close Tab":
               closeTab(activeIndex);
               break;
             default:
@@ -76,66 +79,83 @@ const EditorPage = () => {
         onToggleSpellCheck={() => setSpellCheckEnabled(!spellCheckEnabled)}
       />
 
-      <TabGroup selectedIndex={activeIndex} onChange={setActiveIndex} className="flex flex-col flex-1">
+      <TabGroup
+        selectedIndex={activeIndex}
+        onChange={setActiveIndex}
+        className="flex flex-col flex-1"
+      >
         <div className="flex items-center border-border px-1">
-          <div ref={tabListRef} className="flex-1 overflow-x-auto scrollbar-small" onWheel={handleScroll}>
-          <TabList className="flex space-x-2 focus:outline-none pt-1">
-            {tabs.map((tab: TabData, index: number) => (
-              <Tab
-                key={tab.id}
-                className={({ selected }) =>
-                  `flex-shrink-0 tab-width px-2 py-2 text-sm rounded-t focus:outline-none ${
-                    selected
-                      ? "bg-primary text-primary-foreground hover:bg-destructive"
-                      : "bg-secondary text-secondary-foreground hover:bg-muted"
-                  }`
-                }
-              >
-                {({ selected }) => (
-                  <div
-                    className="flex items-center justify-between"
-                    title={tab.filePath || tab.title}
-                  >
-                    <div className="truncate flex-1">{tab.title}</div>
-                    <div className={`mx-1 pb-0.5  ${
-                        selected
-                          ? "text-red-200"
-                          : "text-red-500"
-                      }`}
-                     >{!tab.isSaved && "●"}</div>
-                    <span
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        closeTab(index);
-                      }}
-                      className={` ${
-                        selected
-                          ? "text-red-200 hover:text-red-600"
-                          : "text-red-500 hover:text-red-700"
-                      }`}
-                      role="button"
-                      aria-label="Close Tab"
+          <div
+            ref={tabListRef}
+            className="flex-1 overflow-x-auto scrollbar-small"
+            onWheel={handleScroll}
+          >
+            <TabList className="flex space-x-2 focus:outline-none pt-1">
+              {tabs.map((tab: TabData, index: number) => (
+                <Tab
+                  key={tab.id}
+                  className={({ selected }) =>
+                    `flex-shrink-0 tab-width px-2 py-2 text-sm rounded-t focus:outline-none ${
+                      selected
+                        ? "bg-primary text-primary-foreground hover:bg-destructive"
+                        : "bg-secondary text-secondary-foreground hover:bg-muted"
+                    }`
+                  }
+                >
+                  {({ selected }) => (
+                    <div
+                      className="flex items-center justify-between"
+                      title={tab.filePath || tab.title}
                     >
-                      ✕
-                    </span>
-                  </div>
-                )}
-              </Tab>
-            ))}
-          </TabList>
+                      <div className="truncate flex-1">{tab.title}</div>
+                      <div
+                        className={`mx-1 pb-0.5  ${
+                          selected ? "text-red-200" : "text-red-500"
+                        }`}
+                      >
+                        {!tab.isSaved && "●"}
+                      </div>
+                      <span
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          closeTab(index);
+                        }}
+                        className={` ${
+                          selected
+                            ? "text-red-200 hover:text-red-600"
+                            : "text-red-500 hover:text-red-700"
+                        }`}
+                        role="button"
+                        aria-label="Close Tab"
+                      >
+                        ✕
+                      </span>
+                    </div>
+                  )}
+                </Tab>
+              ))}
+            </TabList>
           </div>
-          <button onClick={() => addTab()} className="w-8 h-8 ml-2 bg-accent text-accent-foreground rounded-xl flex items-center justify-center text-justify hover:bg-destructive">
+          <button
+            onClick={() => addTab()}
+            className="w-8 h-8 ml-2 bg-accent text-accent-foreground rounded-xl flex items-center justify-center text-justify hover:bg-destructive"
+          >
             +
           </button>
         </div>
         <TabPanels className="flex-1 flex flex-col h-full">
           {tabs.map((tab: TabData, index: number) => (
-            <TabPanel key={tab.id} className="relative flex-1 flex flex-col bg-popover text-popover-foreground">
+            <TabPanel
+              key={tab.id}
+              className="relative flex-1 flex flex-col bg-popover text-popover-foreground"
+            >
               <SearchBar
                 isVisible={isSearchVisible}
                 content={tab.content}
                 onHighlight={highlightMatch}
-                onContentReplace={(updatedContent) => { updateTabContent(activeIndex, updatedContent); }}
+                onContentReplace={(updatedContent) => {
+                  updateTabContent(activeIndex, updatedContent);
+                }}
                 onClose={() => setSearchVisible(false)}
               />
 
